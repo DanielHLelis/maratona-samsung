7d3114863c3bc671d6d0530cc25aa66e46207d8c
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2018-09-16 19:15:11 -0300

    Gatinho Fofo

--- AppRN/Alpha Estudos/src/config/images.js
+++ AppRN/Alpha Estudos/src/config/images.js
@@ -1,5 +1,7 @@
 const IMAGES = {
-    LOGO: require('@assets/images/1by1.png'),
+    // LOGO: require('@assets/images/1by1.png'),
+    LOGO: {uri: 'https://i.ytimg.com/vi/W-PBFMECvTE/maxresdefault.jpg'},
+    // LOGO: {uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/1200px-Square_-_black_simple.svg.png'},
     SETA: require('@assets/images/Seta.png'),
     OPTIONS: require('@assets/images/Options.png')
 }
--- AppRN/Alpha Estudos/src/screens/AppIntroScreen.js
+++ AppRN/Alpha Estudos/src/screens/AppIntroScreen.js
@@ -6,20 +6,17 @@ import COLORS from '@config/colors';
 import constants from '@config/constants';
 import IMAGES from '@config/images';
 
-/* Componentes - Core - Imports */
-import Label from '@components/core/Label';
 import Button from '@components/core/Button';
-
-import StyleMain from '@styles/StyleMain';
 
 export default class AppIntroScreen extends React.Component {
   render() {
     return (
       <Background>
-        {/* <Logo source= {IMAGES.LOGO} /> */}
+        <Logo source={IMAGES.LOGO} blurRadius={4} />
         <StyledText>
           Alpha{'\n'}Estudos
         </StyledText>
+
         <View style={{ width: '80%' }}>
           <Button marginTop={30} onPress={() => this.props.navigation.navigate('Main')}>
             {/* Entrar */} {this.props.navigation.getParam('name', 'Entrar')}
@@ -37,14 +34,15 @@ let StyledText = styled.Text`
 `;
 
 let Background = styled.View`
-  background-color: #00d8ff;
+  background-color: ${COLORS.blueBackground};
   height: ${Dimensions.get('screen').height};
   align-items: center;
   justify-content: center;
 `;
 
-// let Logo = styled.Image`
-//   width: ${Dimensions.get('screen').width * 0.8};
-//   height: ${Dimensions.get('screen').width * 0.8};
-//   border-radius: 50;
-// `;
+let Logo = styled.Image`
+  width: ${Dimensions.get('screen').width};
+  height: ${Dimensions.get('screen').height};
+  /* border-radius: ${Dimensions.get('screen').width * 0.4}; */
+  position: absolute;
+`;
