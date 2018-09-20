04a931ca66d9d5722a9aee9f74cf49e4cb0e3eb3
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2018-09-20 16:44:40 -0300

    Fix CheckBox, Fix Views, Flat-List Idea

--- AppRN/Alpha Estudos/src/components/Question.js
+++ AppRN/Alpha Estudos/src/components/Question.js
@@ -10,20 +10,47 @@ import {
  import TYPOGRAPHY from '@config/typography';
  import colors from '@config/colors';
 
+import CheckBox from '@components/core/CheckBox'  
+
  class QuestionBox extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        
+        this.state= {
+            options:{},
+            answers: props.answers
+        }
+    }
 
+    createOptions = (options) => {
+        let obj ={};
+        for(let a in options){
+            obj.push({[a]: false});
+        }
+        return obj;
+    }
 
+    componentWillMount(){
+        this.setState({options: this.createOptions(this.props.answers)});
     }
 
     render(){
         return(
-            <View>
+            // <ScrollView>
+                <StyledView style={{flex: 1}}>
 
-            </View>
+                    {/*Flat-list por item busca answer por item, (var in a)*/}
+                    <CheckBox  checked={this.state.checked} callBack={(a) => this.setState({checked: a})} />
+                    <CheckBox  checked={this.state.checked} callBack={(a) => this.setState({checked: a})} />    
+                </StyledView>
+            // </ScrollView>
+                
         );
     }
- }
+}
 
- 
+ let StyledView = styled.View`
+    flex: 1;
+ `;
+
+ export default QuestionBox;
