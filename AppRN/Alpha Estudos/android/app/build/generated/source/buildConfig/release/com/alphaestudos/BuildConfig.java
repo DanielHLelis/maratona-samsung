0d35bb7c80f1d56cd6bf95745eb0e3d47099508b
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2018-11-10 01:02:59 -0200

    Updated dependencies

--- AppRN/Alpha Estudos/android/app/build/generated/source/buildConfig/release/com/alphaestudos/BuildConfig.java
+++ AppRN/Alpha Estudos/android/app/build/generated/source/buildConfig/release/com/alphaestudos/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.alphaestudos";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 71;
-  public static final String VERSION_NAME = "0.1.8";
+  public static final int VERSION_CODE = 72;
+  public static final String VERSION_NAME = "0.1.9";
 }
